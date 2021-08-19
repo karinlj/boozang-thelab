fc86f799d5382c5c201230af05de02e93f841050
ljunggren <mljunggren@gmail.com>
2021-08-18 21:06:18 -0400

    Trigger CI run

--- reporter.js
+++ reporter.js
@@ -1,7 +1,7 @@
 const report = require('multiple-cucumber-html-reporter');
  
 report.generate({
-    jsonDir: './json',
+    jsonDir: '.',
     reportPath: './public',
     metadata:{
         browser: {
